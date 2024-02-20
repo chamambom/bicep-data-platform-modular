e5d4c90351c719ee8c9aa8b0b92bd0d08b98f8a9
Martin Chamambo <chamambom@gmail.com>
2024-02-20 16:23:05 +1300

    Changes to be committed:

--- staging-deploy.ps1
+++ staging-deploy.ps1
@@ -1,10 +1,6 @@
 $resourceGroup = 'rg-dev-mdp-01'
-
-
 az deployment group create -f ./main.bicep  -g $resourceGroup -p .\dev.bicepparam
 
-
-# az deployment sub create -n 'deployRG' -l uksouth -f .\main.bicep -p .\prod.bicepparam 
 
 
 
